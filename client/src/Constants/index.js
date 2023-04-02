b4a05b6627d3f5bf213d1736acd5601f816cebca
aliakbarlive <786akbara@gmail.com>
2023-04-02 02:30:36 -0600

    add surprise prompt

---
+++ client/src/Constants/index.js
@@ -0,0 +1,9 @@
+export const surprisePromp = [
+  "You wake up one morning to find that you have the power to speak and understand every language in the world.",
+  "What do you do with this newfound ability",
+  "You discover a hidden room in your house that you never knew existed",
+  "What do you find inside and how do you react",
+  "You're on a hike and stumble upon a group of people dressed in medieval clothing, who are engaged in a battle with swords and shields. What do you do",
+  "You wake up to find that you have been transported to a different time period. What time period is it and how do you navigate this new world",
+  "You discover that you have the ability to time-travel, but you can only visit one moment in history. Where do you go and why",
+];
