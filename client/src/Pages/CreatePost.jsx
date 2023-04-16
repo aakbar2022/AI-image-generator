6c675882d8bae705bc8e7d6357363d975b38c7ef
aliakbarlive <786akbara@gmail.com>
2023-04-16 01:11:47 -0600

    updated handle create post change

--- client/src/Pages/CreatePost.jsx
+++ client/src/Pages/CreatePost.jsx
@@ -1,39 +1,64 @@
-import React, { useState } from "react";
-import { useNavigate } from "react-router-dom";
-import { FormField, Loader } from "../Components";
-import { getRandomPrompt } from "../utils";
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+import { preview } from '../assets';
+import { getRandomPrompt } from '../utils';
+import { FormField, Loader } from '../components';
 
 const CreatePost = () => {
   const navigate = useNavigate();
   const [loading, setLoading] = useState(false);
   const [form, setForm] = useState({
-    name: "",
-    prompt: "",
-    photo: "",
+    name: '',
+    prompt: '',
+    photo: '',
   });
-const handleChange=()=>{
-  
-}
+
+      }
 const handleSurpriseMe=()=>{
-  
-}
-  return (<>
-  <section className="create_post_container">
-  <div>
-          <h1>Create</h1>
-          <p className="browser_collection">
-            imaginative stunning images  by Dall-E AI.
-          </p>
-        </div>
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (form.prompt && form.photo) {
+      setLoading(true);
+      try {
+        const response = await fetch('https://dalle-arbb.onrender.com/api/v1/post', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ ...form }),
+        });
+
+        await response.json();
+        alert('Success');
+        navigate('/');
+      } catch (err) {
+        alert(err);
+      } finally {
+        setLoading(false);
+      }
+    } else {
+      alert('Please generate an image with proper details');
+    }
+  };
+
+  return (
+    <section className="max-w-7xl mx-auto">
+      <div>
+        <h1 className="font-extrabold text-[#222328] text-[32px]">Create</h1>
+        <p className="mt-2 text-[#666e75] text-[14px] max-w-[500px]">Generate an imaginative image through DALL-E AI and share it with the community</p>
+      </div>
         <FormField value={form.name}
         placeholder="What do you do with this newfound ability"
-        handleChange={handleChange}
+            handleChange={handleChange}
         type='text'
         isSurprise
-        handleSurpriseMe={handleSurpriseMe}
-        />
-  </section>
-  </>);
+            handleSurpriseMe={handleSurpriseMe}
+          />
+    </section>
+  );
 };
 
 export default CreatePost;
