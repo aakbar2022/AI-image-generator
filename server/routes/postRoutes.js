0dcb980f6f3b7d9c52cfc1d9bafad876f76c0dbf
Ali Akbar <115136332+aakbar2022@users.noreply.github.com>
2023-09-04 13:07:50 +0500

    cages

--- server/routes/postRoutes.js
+++ server/routes/postRoutes.js
@@ -20,6 +20,7 @@ router.route('/').get(async (req, res) => {
     res.status(200).json({ success: true, data: posts });
   } catch (err) {
     res.status(500).json({ success: false, message: 'Fetching posts failed, please try again' });
+    console.error(err)
   }
 });
 
